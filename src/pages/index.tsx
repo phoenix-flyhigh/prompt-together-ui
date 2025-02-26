2ddc82e4d4553a5032a0f7da313cf3a1881b5253
phoenix-flyhigh <j.akshaya07@gmail.com>
2025-02-26 19:03:02 +0530

    add create and join session btns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,86 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { io } from "socket.io-client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
+  const socket = io("http://localhost:8080");
+
+  const router = useRouter();
+  const { sessionId } = router.query;
+
+  const [sessionInput, setSessionInput] = useState("");
+
+  const updateSessionId = (id: string | undefined) => {
+    const query = { ...router.query, sessionId: id };
+
+    router.push({ pathname: router.pathname, query }, undefined, {
+      shallow: true,
+    });
+  };
+
+  const joinSession = () => {
+    socket.emit(
+      "join room",
+      { roomId: sessionInput },
+      ({ success, roomId }: { success: boolean; roomId: string }) => {
+        if (success) {
+          updateSessionId(roomId);
+        }
+      }
+    );
+  };
+
+  const createSession = () => {
+    socket.emit(
+      "createRoom",
+      ({ success, roomId }: { success: boolean; roomId: string }) => {
+        if (success) {
+          updateSessionId(roomId);
+        }
+      }
+    );
+  };
+
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Prompt Together
+      <main className="flex flex-col gap-8">
+        <h1>Prompt Together</h1>
+        {sessionId ? (
+          <div>Your session is {sessionId}</div>
+        ) : (
+          <>
+            <button
+              className="text-white border-white border-2 rounded-md p-4"
+              onClick={createSession}
+            >
+              Start session
+            </button>
+
+            <div>
+              <input
+                type="text"
+                placeholder="Enter session id"
+                value={sessionInput}
+                onChange={(e) => setSessionInput(e.target.value.trim())}
+                className="text-black w-40 p-2 rounded-md"
+              />
+              <button
+                className="text-white border-white border-2 rounded-md p-4"
+                onClick={joinSession}
+              >
+                Join session
+              </button>
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
