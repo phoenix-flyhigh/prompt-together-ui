12d955a1a4ea2d161aafd9c284942a32fd30925f
phoenix-flyhigh <j.akshaya07@gmail.com>
2025-04-10 12:56:36 +0530

    fix: chat api error

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { GoogleGenAI } from "@google/genai";
 
-// const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY });
 
 type SuccessResponse = {
   response: string;
@@ -24,19 +24,18 @@ export default async function POST(
 
     console.log(prompt);
     
-    // const response = await ai.models.generateContent({
-    //   model: "gemini-2.0-flash",
-    //   contents: prompt,
-    // });
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: prompt,
+    });
     
-    // if (!response || !response.text) {
-    //   res.status(500).json({ message: "No response body from DeepSeek" });
-    //   return;
-    // }
+    if (!response || !response.text) {
+      res.status(500).json({ message: "No response body from DeepSeek" });
+      return;
+    }
     
     console.log("successfully got AI response");
-    // return res.status(200).json({ response: response.text});
-    return res.status(200).json({ response: "random text"});
+    return res.status(200).json({ response: response.text});
   } catch (error) {
     console.error("Error handling request:", error);
     if (!res.headersSent) {
